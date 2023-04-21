808a925ad39ed6cd3b1ebae8f8f59fb149745e30
BaltaKlixJones <balta98_@hotmail.com>
2023-04-21 15:27:29 -0300

    Netflix page Home with movies

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,37 @@
+import useCurrentUser from '@/hooks/useCurrentUser'
+import { NextPageContext } from 'next'
+import { getSession} from 'next-auth/react'
+import NavBar from '@/components/NavBar'
+import BillBoard from '@/components/BillBoard'
+import MovieList from '@/components/MovieList'
+import useMoviesList from '@/hooks/useMoviesList'
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context)
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      },
+    }
+  }
+  return {
+    props: {},
+  }
+}
 
 export default function Home() {
+  const { data: movies = [] } = useMoviesList()
+
   return (
    <>
-   <h1 className="text-2xl text-green-500">Netflix Clone</h1>
+   <NavBar/>
+   <BillBoard />
+   <div className='pb-40'>
+
+   <MovieList title="Trending now" data={movies}/>
+   </div>
    </>
   )
 }
